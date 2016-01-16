5cdcae9cd708043798c4227a7f5051f4ee67dd44
leon <leon@leondeMacBook-Pro.local>
2016-01-16 16:38:58 +0800

    leon commit

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,7 @@
 public class Welcome{
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Java!\n");
+		System.out.println("Welcome to Java!\n");tttt
 		System.out.println("This is on branch!\n");
 	}
 }

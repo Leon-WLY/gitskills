533e32be68076f3862d305750163f41fc8effd0b
leon <leon@leondeMacBook-Pro.local>
2016-01-24 15:04:40 +0800

    fix bug 101

--- Welcome.java
+++ Welcome.java
@@ -2,8 +2,8 @@ public class Welcome{
 	public static void main(String[] args)
 	{
  
-		System.out.println("Welcome to Java!\n");tttt
- 
+		System.out.println("Welcome to Java!\n");
 		System.out.println("This is on branch!\n");
+		System.out.println("Bug is fixed!\n");
 	}
 }
